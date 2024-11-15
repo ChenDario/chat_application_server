fec33263b5d44729e2d0da792ea45783904b3d3d
cosimobassi646 <bassi.cosimo@itismeucci.com>
2024-11-15 15:47:13 +0100

    modifiche alla gestione delle eccezioni

--- src/main/java/com/example/ChatApplicationThread.java
+++ src/main/java/com/example/ChatApplicationThread.java
@@ -16,7 +16,7 @@ public class ChatApplicationThread extends Thread {
     private ArrayList<Group> groups;    
     private ArrayList<String> generated_group_codes;
 
-    public ChatApplicationThread(Socket s, ArrayList<ChatApplicationThread> clients, ArrayList<Group> groups, ArrayList<String> generated_group_codes) throws IOException{
+    public ChatApplicationThread(Socket s, ArrayList<ChatApplicationThread> clients, ArrayList<Group> groups, ArrayList<String> generated_group_codes) throws IOException {
         this.socketClient = s;
         this.clients = clients;
         this.groups = groups; 
@@ -26,37 +26,44 @@ public class ChatApplicationThread extends Thread {
     }
 
     @Override
-    public void run(){
+    public void run() {
+        try {
+            // Validazione dell'username
+            username_validation(in, out);
 
+            // Ricevi richieste dal client finché non si disconnette
+            UserRequest.receive_user_requests(in, out, groups, clients, userName, generated_group_codes);
+
+        } catch (IOException e) {
+            System.err.println("Errore nella comunicazione con il client: " + e.getMessage());
+        } finally {
             try {
-                //Validazione dell'username
-                username_validation(in, out);
-
-                //Continue to receive message from the user until he exit
-                //Rendere la classe UserRequest non statica(implementazione del costruttore)
-                UserRequest.receive_user_requests(in, out, groups, clients, userName, generated_group_codes);
-
-                System.out.println("Client Disconesso");
-            } catch (IOException e) {
-                // TODO: handle exception
-                e.printStackTrace();
-            } finally {
-                try {
+                // Chiusura delle risorse
+                if (socketClient != null && !socketClient.isClosed()) {
                     socketClient.close();
-                    clients.remove(this); // Rimuovi il client dalla lista quando si disconnette
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                System.err.println("Errore durante la chiusura delle risorse: " + e.getMessage());
+            } finally {
+                // Rimuovi il client dalla lista dopo la chiusura
+                clients.remove(this);
+                System.out.println("Client disconnesso e rimosso dalla lista.");
             }
+        }
     }
 
-    //Metodo get
-    public String getUserName(){
+    public String getUserName() {
         return this.userName;
     }
 
-    public void username_validation(BufferedReader in, DataOutputStream out) throws IOException{
-        //Validazione dell'username
+    public void username_validation(BufferedReader in, DataOutputStream out) throws IOException {
+        // Validazione dell'username
         String x = "";
         do {
             x = UsernameIdentification.username_status(in, clients);
@@ -74,6 +81,4 @@ public class ChatApplicationThread extends Thread {
     public BufferedReader getIn() {
         return this.in;
     }
-
-
 }
